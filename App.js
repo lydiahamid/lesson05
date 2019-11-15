0c88ebda471c6e3463aebd0350cf2190467d4d8b
Lydia Hamid <lydia_hamid@rp.edu.sg>
2019-11-15 13:58:16 +0800

    Initial commit

---
+++ App.js
@@ -0,0 +1,16 @@
+/**
+ * Lesson 05 Activities
+ */
+
+import React from 'react';
+import {View} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+
+    </View>
+  );
+};
+
+export default App;
